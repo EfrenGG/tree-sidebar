9d2217676804ac2278cf380a97967cd01daf59b8
Efren García Guarneros <efren.garcia.guarneros@gmail.com>
2020-10-02 22:54:53 -0500

    Carga y renderizado de productos

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { MenuItem } from '../menu-item';
+import { MenuItemsService } from '../menu-items.service';
+import { Link, Product } from '../models';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,19 +12,36 @@ import { MenuItem } from '../menu-item';
 })
 export class ProductDetailComponent implements OnInit, OnDestroy {
   private routeDataSub: Subscription;
-  productsLink: string;
-  products: Array<any>;
-  title: string;
+  private productDataSub: Subscription;
 
-  constructor(private route: ActivatedRoute) {}
+  title: string;
+  products: Product[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private menuService: MenuItemsService
+  ) {}
 
   state$: Observable<object>;
 
   ngOnInit() {
     this.routeDataSub = this.route.data.subscribe(
       (data: { menuOption: MenuItem }) => {
+        this.products = [];
         const { menuOption } = data;
-        console.log(menuOption);
+        if (!menuOption) {
+          return;
+        }
+        this.title = menuOption.name;
+        const { href } = menuOption.links.find(
+          (link) => link.rel === 'products'
+        );
+        this.productDataSub = this.menuService
+          .getProducts(href)
+          .subscribe(
+            (data: { links: Link[]; content: Product[] }) =>
+              (this.products = data.content.slice(1, 10))
+          );
       }
     );
   }
@@ -30,6 +49,9 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     if (this.routeDataSub) {
       this.routeDataSub.unsubscribe();
+    }
+    if (this.productDataSub) {
+      this.productDataSub.unsubscribe();
     }
   }
 }
